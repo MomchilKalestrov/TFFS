ea7eab0ff1af153027b2f6cb28439339b653ab9d
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-10-01 12:03:09 +0300

    Fixed the dumbass jump function. Added a skybox

--- scripts/movement.gd
+++ scripts/movement.gd
@@ -11,44 +11,44 @@ extends CharacterBody3D
 var target_velocity = Vector3.ZERO
 
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-	if Input.is_action_pressed("move_back"):
-		direction.z += 1
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
-	if Input.is_action_pressed("move_left"):
-		direction.x -= 1
-	if Input.is_action_pressed("move_right"):
-		direction.x += 1
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		target_velocity.y = jump_force
-	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		direction = direction.rotated(Vector3.UP, rot_x)
-		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
+    var direction = Vector3.ZERO
+    if Input.is_action_pressed("move_back"):
+        direction.z += 1
+    if Input.is_action_pressed("move_forward"):
+        direction.z -= 1
+    if Input.is_action_pressed("move_left"):
+        direction.x -= 1
+    if Input.is_action_pressed("move_right"):
+        direction.x += 1
+    
+    if direction != Vector3.ZERO:
+        direction = direction.normalized()
+        direction = direction.rotated(Vector3.UP, rot_x)
+        
+    target_velocity.x = direction.x * speed
+    target_velocity.z = direction.z * speed
 
-	# Vertical Velocity
-	if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-		target_velocity.y -= fall_acceleration * delta
-	else:
-		target_velocity.y = 0
+    # Vertical Velocity
+    if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
+        target_velocity.y -= fall_acceleration * delta
+    else:
+        target_velocity.y = 0
+        if Input.is_action_just_pressed("jump"):
+            target_velocity.y = jump_force
 
-	# Moving the Character
-	velocity = target_velocity
-	move_and_slide()
-	
+    # Moving the Character
+    velocity = target_velocity
+    move_and_slide()
+    
 func _input(event):
-	if event is InputEventMouseMotion:
-		rot_x -= event.relative.x * mouse_sens
-		rot_y -= event.relative.y * mouse_sens
-		
-		rot_y = clamp(rot_y, -PI / 2, PI / 2)
-		
-		$Pivot.rotation_degrees.y = rad_to_deg(rot_x)
-		$Pivot.rotation_degrees.x = rad_to_deg(rot_y)
+    if event is InputEventMouseMotion:
+        rot_x -= event.relative.x * mouse_sens
+        rot_y -= event.relative.y * mouse_sens
+        
+        rot_y = clamp(rot_y, -PI / 2, PI / 2)
+        
+        $Pivot.rotation_degrees.y = rad_to_deg(rot_x)
+        $Pivot.rotation_degrees.x = rad_to_deg(rot_y)
